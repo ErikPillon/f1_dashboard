8a94f794e24c54d2fd483ec4a7067d0238b1ea3d
ErikPillon <erik.pillon@gmail.com>
2024-09-10 23:32:45 +0200

    add all_time stats

--- fetcher.py
+++ fetcher.py
@@ -68,6 +68,111 @@ def fetch_seasons_years():
     return fetch_data_from_query(engine, query)
 
 
+@st.cache_data
+def fetch_number_of_poles_by_driver():
+    query = """SELECT q.driverId, d.forename, d.surname, COUNT(*) as numberPoles FROM qualifying q JOIN drivers d ON d.driverId = q.driverId WHERE q.position = 1 GROUP BY q.driverId ORDER BY numberPoles DESC;"""
+    return fetch_data_from_query(engine, query)
+
+
+@st.cache_data
+def fetch_number_of_wins_by_driver():
+    query = """select r.driverId, d.forename, d.surname, count(*) as numberWins from results r join drivers d on d.driverId = r.driverId where r.position = 1 GROUP BY r.driverId ORDER BY numberWins DESC;"""
+    return fetch_data_from_query(engine, query)
+
+
+def fetch_laps_led_by_driver(year: int = 2024):
+    query = f"""
+        WITH laps_led_per_driver_CTE AS (
+        SELECT
+            lt.driverId,
+            COUNT(*) AS number
+        FROM
+            lapTimes lt
+        WHERE lt.raceId IN (
+            SELECT raceId
+            FROM races r
+            WHERE r.year = {year}
+        )
+        AND lt.position = 1
+        GROUP BY lt.driverId
+    )
+    -- Query to get laps led per driver
+    SELECT
+        d.forename,
+        d.surname,
+        ll_cte.number
+    FROM
+        drivers d
+        JOIN laps_led_per_driver_CTE ll_cte ON ll_cte.driverId = d.driverId
+    ORDER BY
+        ll_cte.number DESC;
+    """
+    return fetch_data_from_query(engine, query)
+
+
+def fetch_fastest_laps_by_driver(year: int = 2024):
+    query = f"""
+        WITH fastest_laps_by_driver_CTE AS (
+        SELECT
+            lt.driverId,
+            COUNT(*) AS number
+        FROM
+            lapTimes lt
+        WHERE lt.raceId IN (
+            SELECT raceId
+            FROM races r
+            WHERE r.year = {year}
+        )
+        AND lt.position = 1
+        GROUP BY lt.driverId
+    )
+    -- Query to get fastest laps per driver
+    SELECT
+        d.forename,
+        d.surname,
+        ll_cte.number
+    FROM
+        drivers d
+        JOIN fastest_laps_by_driver_CTE ll_cte ON ll_cte.driverId = d.driverId
+    ORDER BY
+        ll_cte.number DESC;
+    """
+    return fetch_data_from_query(engine, query)
+
+
+def fetch_number_of_laps_completed_by_driver(year: int = 2024):
+    query = f"""
+    WITH laps_per_driver_CTE AS (
+        SELECT 
+            lt.driverId, 
+            COUNT(*) AS number 
+        FROM 
+            lapTimes lt 
+        WHERE lt.raceId IN (
+            SELECT raceId 
+            FROM races r 
+            WHERE r.year = {year}
+        ) 
+        GROUP BY lt.driverId
+    )
+
+    -- Query to get total laps per driver
+    SELECT 
+        d.forename, 
+        d.surname, 
+        l_cte.number 
+    FROM 
+        drivers d 
+        JOIN laps_per_driver_CTE l_cte ON l_cte.driverId = d.driverId 
+    ORDER BY 
+        l_cte.number DESC;"""
+    return fetch_data_from_query(engine, query)
+
+
+def fetch_number_of_world_titles_by_driver():
+    pass
+
+
 def fetch_data_from_query(engine, query: str):
     with engine.connect() as connection:
         df = pd.read_sql(query, connection)
--- pages/all_time_stats.py
+++ pages/all_time_stats.py
@@ -1,10 +1,15 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
 
 from fetcher import (
-    fetch_cumulative_points_by_constructor,
+    fetch_fastest_laps_by_driver,
+    fetch_number_of_laps_completed_by_driver,
+    fetch_number_of_poles_by_driver,
+    fetch_number_of_world_titles_by_driver,
+    fetch_laps_led_by_driver,
     fetch_cumulative_points_by_driver,
+    fetch_cumulative_points_by_constructor,
+    fetch_seasons_years,
+    fetch_number_of_wins_by_driver,
 )
 
 st.title("All Time Stats")
@@ -13,3 +18,39 @@ st.markdown(
     """This page is dedicated to the all time stats. It displays the total number of points by driver, 
     by constructor, the total number of world titles, total number of poles, total number of wins, and total number of fastest laps."""
 )
+
+season_years = fetch_seasons_years()
+min_year = season_years["year"].min()
+max_year = season_years["year"].max()
+
+st.select_slider(
+    "Select range", options=season_years["year"], value=(min_year, max_year)
+)
+
+st.markdown("## Total number of laps led by driver")
+
+fastest_laps_by_driver = fetch_laps_led_by_driver()
+st.dataframe(fastest_laps_by_driver)
+
+
+st.markdown("## Number of laps completed by driver")
+
+number_of_laps_completed_by_driver = fetch_number_of_laps_completed_by_driver()
+st.dataframe(number_of_laps_completed_by_driver)
+
+
+st.markdown("## number of poles by driver")
+
+number_of_poles_by_driver = fetch_number_of_poles_by_driver()
+st.dataframe(number_of_poles_by_driver)
+
+st.markdown("## number of wins by driver")
+
+number_of_wins_by_driver = fetch_number_of_wins_by_driver()
+st.dataframe(number_of_wins_by_driver)
+
+
+st.markdown("## number_of_world_titles_by_driver")
+
+number_of_world_titles_by_driver = fetch_number_of_world_titles_by_driver()
+st.dataframe(number_of_world_titles_by_driver)
