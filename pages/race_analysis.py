bf4349c869950c85c7fbe59cd4820d2f4738375f
ErikPillon <erik.pillon@gmail.com>
2024-09-23 01:59:19 +0200

    feat: :sparkles: racepace plot

--- pages/race_analysis.py
+++ pages/race_analysis.py
@@ -2,7 +2,7 @@ import streamlit as st
 from colors import Colors
 
 from fetcher import fetch_seasons_years, fetch_championship_gp, fetch_gp_laps
-from viz_utils.line_plots import plot_race_progression
+from viz_utils.line_plots import plot_race_progression, plot_race_pace_progression
 
 st.title("Race Analysis")
 
@@ -31,12 +31,12 @@ laps = fetch_gp_laps(year=year_selected, round=round_selected)
 championship_gp = championship_rounds[championship_rounds["round"] == round_selected]
 
 st.markdown(f"""
-            #### Brief Info
-            
-            The {championship_gp.iloc[0]["name"]} was round {championship_gp.iloc[0]["round"]} of the {year_selected} F1 Championship and was held at {championship_gp.iloc[0]["circuit"]} in {championship_gp.iloc[0]["location"]} ({championship_gp.iloc[0]["country"]}) on {championship_gp.iloc[0]["date"]}.
-            
-            The GP saw the participation of {laps["code"].unique().shape[0]} drivers and a total of {laps.shape[0]} laps were completed.
-            """)
+    #### Brief Info
+    
+    The {championship_gp.iloc[0]["name"]} was round {championship_gp.iloc[0]["round"]} of the {year_selected} F1 Championship and was held at {championship_gp.iloc[0]["circuit"]} in {championship_gp.iloc[0]["location"]} ({championship_gp.iloc[0]["country"]}) on {championship_gp.iloc[0]["date"]}.
+    
+    The GP saw the participation of {laps["code"].unique().shape[0]} drivers and a total of {laps.shape[0]} laps were completed.
+    """)
 
 st.markdown("""
     ## GP Race Pace Analysis
@@ -44,9 +44,21 @@ st.markdown("""
     Select a driver (resp. multiple drivers) below to see its (resp. their) race pace progression over the GP.""")
 
 
-colors = Colors(year=year_selected, round=round_selected)
+@st.cache_data
+def get_colors(year, round):
+    return Colors(year=year, round=round)
+
+
+colors = get_colors(year=year_selected, round=round_selected)
+
 fig = plot_race_progression(laps, colors=colors)
 
 st.plotly_chart(fig)
 
-# driver_selected = st.multiselect("Select driver", laps["code"].unique())
+driver_selected = st.multiselect(
+    "Select driver", laps["code"].unique(), default=laps["code"].unique()
+)
+
+fig = plot_race_pace_progression(laps, driver_selected, colors=colors)
+
+st.plotly_chart(fig)
--- viz_utils/line_plots.py
+++ viz_utils/line_plots.py
@@ -1,7 +1,14 @@
+import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-import fastf1 as ff1
 from colors import Colors
+
+
+def ms_to_mmssmmm(ms: int) -> str:
+    minutes = ms // 60000
+    seconds = (ms % 60000) // 1000
+    milliseconds = ms % 1000
+    return f"{minutes:02}:{seconds:02}:{milliseconds:03}"
 
 
 def plot_race_progression(df: pd.DataFrame, colors: Colors) -> go.Figure:
@@ -53,3 +60,78 @@ def plot_race_progression(df: pd.DataFrame, colors: Colors) -> go.Figure:
 
     # Show the plot
     return fig
+
+
+def plot_race_pace_progression(
+    df: pd.DataFrame, drivers: list = [], colors: Colors = None
+) -> go.Figure:
+    """_summary_
+
+    Args:
+        df (pd.DataFrame): _description_
+        drivers (list): _description_
+        colors (Colors): _description_
+
+    Returns:
+        go.Figure: _description_
+    """
+    assert "code" in df.columns, "The DataFrame must contain a 'code' column"
+    assert "lap" in df.columns, "The DataFrame must contain a 'lap' column"
+    assert (
+        "milliseconds" in df.columns
+    ), "The DataFrame must contain a 'milliseconds' column"
+
+    drivers = drivers or df["code"].unique()
+
+    # Create the figure object
+    fig = go.Figure()
+
+    # Plot each driver as a separate line
+    for driver in drivers:
+        driver_data = df[df["code"] == driver]
+
+        driver_data["milliseconds_ma"] = driver_data["milliseconds"].rolling(3).mean()
+
+        driver_data["cleaned_times"] = driver_data["milliseconds"].where(
+            driver_data["milliseconds"] <= driver_data["milliseconds_ma"] * 1.1,
+            other=None,
+        )
+
+        # driver_data["formatted_times"] = driver_data["cleaned_times"].apply(
+        #     lambda x: ms_to_mmssmmm(x) if not pd.isna(x) else np.nan
+        # )
+
+        # lap_progression = clean_lap_progression(driver_data["milliseconds"].values)
+
+        fig.add_trace(
+            go.Scatter(
+                x=driver_data["lap"],
+                y=driver_data["cleaned_times"],
+                mode="lines+markers",
+                name=driver,
+                line_shape="linear",
+                line=dict(color=colors.get_driver_color(driver)),
+            )
+        )
+
+    # Update the layout for the chart
+    fig.update_layout(
+        title="Race Pace Comparison",
+        xaxis_title="Lap",
+        yaxis_title="Lap Time (mm:ss:mmm)",
+        yaxis_tickformat="%M:%S.%3f",
+        legend_title="Driver",
+        hovermode="x unified",
+        plot_bgcolor="rgba(240,240,240,0.8)",  # Light background similar to the image
+        font=dict(size=12),
+    )
+
+    return fig
+
+
+if __name__ == "__main__":
+    from fetcher import fetch_gp_laps
+
+    laps = fetch_gp_laps(year=2024, round=10)
+
+    fig = plot_race_pace_progression(laps)
